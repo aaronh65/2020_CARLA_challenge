688874b0ec080a415aa1cc65bc4a34faac249772
Aaron Huang <aaronhua@trinity.vision.cs.cmu.edu>
2020-12-02 19:01:43 -0500

    pushing new config and log file structure

--- run_agent_cluster.sh
+++ run_agent_cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source /home/aaron/.bashrc
-conda activate leaderboard
+source /home/aaronhua/.bashrc
+conda activate lb
 
 # Python env variables so the subdirectories can find each other
 export CUDA_VISIBLE_DEVICES=$1
@@ -9,10 +9,11 @@ export PORT=$2
 export ROUTES=$3
 export LOGDIR=$4
 export TM_PORT=$5
+export REPETITIONS=$8
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/aaronhua/anaconda3/lib
-export CARLA_ROOT=/home/aaron/workspace/carla/CARLA_0.9.10.1
-export LBC_ROOT=/home/aaron/workspace/carla/2020_CARLA_challenge
+export CARLA_ROOT=/home/aaronhua/CARLA_0.9.10.1
+export LBC_ROOT=/home/aaronhua/2020_CARLA_challenge
 export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla
 export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
 export PYTHONPATH=$PYTHONPATH:leaderboard
@@ -26,6 +27,7 @@ export TEAM_AGENT=$LBC_ROOT/leaderboard/team_code/${6}.py
 export TEAM_CONFIG=$LBC_ROOT/leaderboard/data/$7
 
 # leaderboard config
+
 
 if [ -d "$TEAM_CONFIG" ]; then
     CHECKPOINT_ENDPOINT="$LOGDIR/$(basename $ROUTES .xml).txt"
@@ -42,7 +44,8 @@ python leaderboard/leaderboard/leaderboard_evaluator.py \
 --checkpoint=${CHECKPOINT_ENDPOINT} \
 --port=${PORT} \
 --trafficManagerPort=${TM_PORT} \
---debug=${DEBUG_CHALLENGE}
+--debug=${DEBUG_CHALLENGE} \
+--repetitions=${REPETITIONS}
 
 echo "Done. See $CHECKPOINT_ENDPOINT for detailed results."
 
