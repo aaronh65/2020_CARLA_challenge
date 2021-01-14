aa5503d32e66c730b1d1572fa4ae0d25e43d5136
aaron <aaronh65@gmail.com>
2021-01-13 18:22:02 -0800

    cleanup dense waypoint generation from rl trainer

--- leaderboard/team_code/rl/trainer.py
+++ leaderboard/team_code/rl/trainer.py
@@ -34,38 +34,6 @@ def train(args):
 
         # save model if applicable
 
-def load_and_wait_for_world(config, world, ego_vehicles=None):
-    settings = world.get_settings()
-    settings.fixed_delta_seconds = 1.0 / 20
-    settings.synchronous_mode = True
-
-def generate_dense_waypoints(args):
-    from leaderboard.utils.route_manipulation import interpolate_trajectory
-    root = '/home/aaron/workspace/carla/2020_CARLA_challenge'
-    split = args.routes.split('/')[-1]
-    split = split.split('_')[1].split('.')[0]
-
-    client = carla.Client('localhost', 2000)
-    
-    route_indexer = RouteIndexer(args.routes, args.scenarios, args.repetitions)
-    while route_indexer.peek():
-        config = route_indexer.next()
-        world = client.load_world(config.town)
-        cmap = world.get_map()
-        gps_route, route = interpolate_trajectory(world, config.trajectory)
-        route_vectors = np.asarray([transform_to_vector(elem[0]) for elem in route])
-        #route = convert_transform_to_location(route)
-        #waypoints = [cmap.get_waypoint(elem[0]) for elem in route]
-        #draw_waypoints(world, waypoints)
-
-        route_id = int(config.name.split('_')[1])
-        route_id = f'{route_id:02d}'
-        save_path = f'{root}/leaderboard/data/routes_{split}/route_{route_id}_{config.town}_dense.npy'
-        with open(save_path, 'wb') as f:
-            np.save(f, route_vectors)
-        print(save_path)
-
-
 def main():
 
     parser = argparse.ArgumentParser()
